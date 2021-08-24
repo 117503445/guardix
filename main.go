019771492fbfa8434a2d8d1d500dedc4e320b392
117503445 <t117503445@gmail.com>
2021-08-24 23:58:01 +0800

    feat: support push

--- main.go
+++ main.go
@@ -3,16 +3,24 @@ package main
 import (
 	"fmt"
 	"github.com/spf13/viper"
+	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 )
 
-func serverChanGet(serverChanKey string, hour int) {
+func push(name string, hour int) {
+	fmt.Println(time.Unix(time.Now().Unix(), 0).Format("2006-01-02 15:04:05"))
 	httpClient := &http.Client{}
-	_, err := httpClient.Get(fmt.Sprintf("https://sc.ftqq.com/%s.send?text=电脑已开机%d小时", serverChanKey, hour))
+	url := fmt.Sprintf("https://push.gh.117503445.top:20000/push/text/v1?name=%s", name)
+	fmt.Println(url, hour)
+	r, err := httpClient.Post(url, "application/json", strings.NewReader(fmt.Sprintf("电脑已开机%d小时", hour)))
+	b, _ := ioutil.ReadAll(r.Body)
+	fmt.Println(string(b))
 	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Println()
 }
 
 func main() {
@@ -22,10 +30,13 @@ func main() {
 	if err != nil {
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
-	serverChanKey := viper.GetString("server_chan")
+	name := viper.GetString("name")
 	hour := 0
+	push(name, hour)
+
 	startTime := time.Now()
 	maxTime := 3600 // 1 hour
+
 	for true {
 		time.Sleep(time.Second)
 		duration := int(time.Now().Sub(startTime))
@@ -36,18 +47,18 @@ func main() {
 				maxTime = 3600 * 2
 				startTime = time.Now()
 				hour++
-				serverChanGet(serverChanKey, hour)
+				push(name, hour)
 				println("end 1 hour")
 			} else if maxTime == 3600*2 {
 				maxTime = 3600 * 4
 				startTime = time.Now()
 				hour += 2
-				serverChanGet(serverChanKey, hour)
+				push(name, hour)
 				println("end 2 hour")
 			} else if maxTime == 3600*4 {
 				startTime = time.Now()
 				hour += 4
-				serverChanGet(serverChanKey, hour)
+				push(name, hour)
 				println("end 4 hour")
 			}
 		}
